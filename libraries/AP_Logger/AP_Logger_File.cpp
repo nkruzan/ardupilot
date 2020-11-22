a6c6cc023902ece3c2a4fb5a3eb505e7cceee7c2
Buzz <davidbuzz@gmail.com>
2020-11-22 20:36:25 +1000

    AP_Logger:  longer timeout for esp32 under freertos

--- libraries/AP_Logger/AP_Logger_File.cpp
+++ libraries/AP_Logger/AP_Logger_File.cpp
@@ -962,6 +962,9 @@ bool AP_Logger_File::io_thread_alive() const
 {
     // if the io thread hasn't had a heartbeat in a full seconds then it is dead
     // this is enough time for a sdcard remount
+#if HAL_BOARD_NAME == "ESP32" 
+    return (AP_HAL::millis() - _io_timer_heartbeat) < 10000U || !hal.scheduler->is_system_initialized();
+#endif 
     return (AP_HAL::millis() - _io_timer_heartbeat) < 3000U || !hal.scheduler->is_system_initialized();
 }
 
