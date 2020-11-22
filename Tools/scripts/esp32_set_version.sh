117536b0b7fd7c235b33e098e41dd214ac8c3831
Buzz <davidbuzz@gmail.com>
2020-11-22 21:16:01 +1000

    Tools: add esp32 support

---
+++ Tools/scripts/esp32_set_version.sh
@@ -0,0 +1,5 @@
+PWD=`pwd`
+#  define the version we're building in version.txt for the IDF, or it gets it wrong:
+grep THISFIRMWARE ../../ArduPlane/version.h | cut -d\" -f 2 | sed -e "s/\s/-/g" >  ../../libraries/AP_HAL_ESP32/plane/version.txt
+grep THISFIRMWARE ../../ArduCopter/version.h | cut -d\" -f 2 | sed -e "s/\s/-/g" >  ../../libraries/AP_HAL_ESP32/copter/version.txt
+grep THISFIRMWARE ../../Rover/version.h | cut -d\" -f 2 | sed -e "s/\s/-/g" >  ../../libraries/AP_HAL_ESP32/rover/version.txt
