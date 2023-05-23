7a1e9a285cefc778f86d8f8393dcf54673354331
Nicholas Kruzan <nickkruzan@gmail.com>
2023-05-23 15:06:28 +0000

    AP_HAL_ESP32 add/edit files from buzz repo

---
+++ libraries/AP_HAL_ESP32/EventSource.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <AP_HAL/AP_HAL.h>
+
+#include <stdint.h>
+#include <AP_HAL/AP_HAL_Boards.h>
+#include <AP_HAL/AP_HAL_Macros.h>
+#include <AP_HAL/EventHandle.h>
+#include "AP_HAL_ESP32.h"
+
+#if CH_CFG_USE_EVENTS == TRUE
+class ESP32::EventSource : public AP_HAL::EventSource {
+    // Single event source to be shared across multiple users
+    //AP_HAL::EventSource* get_source() { return evt_src_; }
+public:
+    // generate event from thread context
+    void signal(uint32_t evt_mask) override;
+
+    // generate event from interrupt context
+    void signalI(uint32_t evt_mask) override;
+
+    // Wait on an Event handle, method for internal use by EventHandle
+    bool wait(uint16_t duration_us, AP_HAL::EventHandle* evt_handle) override;
+};
+#endif //#if CH_CFG_USE_EVENTS == TRUE
