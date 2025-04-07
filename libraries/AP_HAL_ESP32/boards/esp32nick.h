456d31bcdb6ac2516c535deddeeba3c8bd54f6a8
Nicholas Kruzan <nickkruzan@gmail.com>
2025-04-07 23:41:25 +0000

    move board subtype to top

--- libraries/AP_HAL_ESP32/boards/esp32nick.h
+++ libraries/AP_HAL_ESP32/boards/esp32nick.h
@@ -15,7 +15,7 @@
 #pragma once
 
 #define HAL_ESP32_BOARD_NAME "esp32-nick"
-// CONFIG_HAL_BOARD_SUBTYPE HAL_BOARD_SUBTYPE_ESP32_NICK
+#define CONFIG_HAL_BOARD_SUBTYPE HAL_BOARD_SUBTYPE_ESP32_NICK
 
 #define TRUE						1
 #define FALSE						0
@@ -34,7 +34,7 @@
 #define PROBE_MAG_IMU_I2C(driver, imudev, bus, addr, args ...) ADD_BACKEND(DRIVER_ ##driver, AP_Compass_ ## driver::probe_ ## imudev(GET_I2C_DEVICE(bus,addr),##args))
 //------------------------------------
 
-#define CONFIG_HAL_BOARD_SUBTYPE HAL_BOARD_SUBTYPE_ESP32_NICK
+
 #define WIFI_CONCAT(x, y) x y
 #define WIFI_PREFIX 			"ardupilot-"
 #define WIFI_SUFFIX 			HAL_ESP32_BOARD_NAME
